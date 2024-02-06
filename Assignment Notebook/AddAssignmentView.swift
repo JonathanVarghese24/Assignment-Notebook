9e71938d7971d3b03eb75f1366ec90f55420818f
Jonathan Varghese <jonathanvarghese24@gmail.com>
2024-02-06 10:02:31 -0600

    Enabled data persistence

---
+++ Assignment Notebook/AddAssignmentView.swift
@@ -0,0 +1,18 @@
+//
+//  AddAssignmentView.swift
+//  Assignment Notebook
+//
+//  Created by Jonathan Varghese on 2/6/24.
+//
+
+import SwiftUI
+
+struct AddAssignmentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddAssignmentView()
+}
---
+++ Assignment Notebook/AssignmentList.swift
@@ -0,0 +1,8 @@
+//
+//  AssignmentList.swift
+//  Assignment Notebook
+//
+//  Created by Jonathan Varghese on 2/5/24.
+//
+
+import Foundation
