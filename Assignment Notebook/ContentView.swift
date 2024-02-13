9dd30cc28672520db73e46241bd1551ecced3231
Jonathan Varghese <jonathanvarghese24@gmail.com>
2024-02-13 10:33:16 -0600

    Final Project

--- Assignment Notebook/ContentView.swift
+++ Assignment Notebook/ContentView.swift
@@ -8,17 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var Assignments = AssignmentList()
+    @State private var showingAddAssignmentView = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach(Assignments.items) { Assignment in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(Assignment.course)
+                                .font(.headline)
+                            Text(Assignment.description)
+                        }
+                        Spacer()
+                        Text(Assignment.dueDate, style: .date)
+                    }
+                }
+                .onMove { indices, newOffset in
+                    Assignments.items.move(fromOffsets: indices, toOffset: newOffset)}
+                
+                .onDelete { indexSet in
+                    Assignments.items.remove(atOffsets: indexSet)}
+            }
+            .sheet(isPresented: $showingAddAssignmentView, content: {
+                AddAssignmentView(assignmentList: Assignments)
+            })
+            .navigationBarTitle("Assignments", displayMode: .inline)
+            .navigationBarItems(leading: EditButton(),
+                                trailing: Button(action: {
+                showingAddAssignmentView = true}) {
+                    Image(systemName: "plus")
+                })
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct Assignment : Identifiable, Codable {
+    var id = UUID()
+    var course = String()
+    var description = String()
+    var dueDate = Date()
 }
